eede640bf4d0652cfc8f82508cead2e1eb1c7078
mayan <mayan@LAPTOP-KQ4R985V>
2020-12-20 23:58:38 +0530

    Design of Home Page.

--- blogistaan/src/main/java/com/blogistaan/controller/NormalController.java
+++ blogistaan/src/main/java/com/blogistaan/controller/NormalController.java
@@ -1,14 +1,15 @@
 package com.blogistaan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class NormalController {
 	@GetMapping("/home")
-	@ResponseBody
-	public String demo() {
-		return "Success";
+	public String demo(Model model) {
+		return "/home";
 	}
 }
