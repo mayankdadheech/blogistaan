c860227d18f7eef226f3394a8e066403d4335095
mayan <mayan@LAPTOP-KQ4R985V>
2020-12-20 21:01:18 +0530

    Done with configuration.

--- blogistaan/src/main/java/com/blogistaan/BlogistaanApplication.java
+++ blogistaan/src/main/java/com/blogistaan/BlogistaanApplication.java
@@ -8,6 +8,7 @@ public class BlogistaanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogistaanApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
